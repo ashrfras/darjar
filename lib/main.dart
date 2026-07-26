13669188fe8ccfe41766779157dcae9b1fd3d515
ashrfras <ghizlanzitouny@gmail.com>
2026-07-26 13:52:35 +0100

    configure authentication and Firestore foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:darjar/app/app.dart';
+import 'package:darjar/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const ProviderScope(child: DarJarApp()));
 }
